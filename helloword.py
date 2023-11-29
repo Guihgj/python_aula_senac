276b169cc5968f5fc4c2cc8e50c122867eab4e0e
Guihgj <guihgj11@outlook.com>
2023-11-29 11:48:31 -0300

    variaveis

---
+++ helloword.py
@@ -0,0 +1,8 @@
+# isso é um comentario ...
+
+"""
+Esse é um comentario com
+varias linhas
+""" 
+
+print("Hello Word")
---
+++ variaveis.py
@@ -0,0 +1,19 @@
+"""""
+str ="textos"
+int = 10
+float = 10.5
+boolean = true
+
+"""
+
+nome="jose"
+print(nome)
+
+print (type(nome))
+#função lenght (espaços tb conta)
+
+print(len(nome))
+
+print (nome.upper())
+for x in nome:
+    print(x)
